fdf262a6abfc266258091b6237f70ea9867ef38d
EmersonPenelli <emersonpenelli@gmail.com>
2023-10-20 11:15:09 -0300

    setup do projeto

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp')
+const sass = require("gulp-sass")(require('sass'));
+// const imagemin = require('gulp-imagemin');
+// const uglify = require('gulp-uglify')
+
+// function scripts() {
+//     return gulp.src('./src/scripts/*.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('./dist/js'))
+// }
+
+// function images() {
+//     return gulp.src('./src/images/**/*') 
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('./dist/images')); 
+// }
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({ outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
+}
+
+exports.default = styles;
+
+// exports.default = gulp.parallel(styles, images, scripts)
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+}
